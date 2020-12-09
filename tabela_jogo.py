089182d0721d1ddbfab50c20a377074c784b1093
Wilson77Calixto <65840622+Wilson77Calixto@users.noreply.github.com>
2020-12-09 14:40:48 -0300

    Add files via upload
    Tabela de jogos  de ida e volta

---
+++ tabela_jogo.py
@@ -0,0 +1,6 @@
+# Programa gera tabela de jogos
+
+for time1 in ('Flamengo', 'Fluminense', 'Botafogo', 'Vasco', 'América'):
+    for time2 in ('Flamengo', 'Fluminense', 'Botafogo', 'Vasco', 'América'):
+        if time1 != time2:
+            print(time1, 'x', time2)
