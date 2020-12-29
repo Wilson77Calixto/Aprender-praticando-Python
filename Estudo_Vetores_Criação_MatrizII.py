569f27e70449b2af0107751ed1c0870300804ca0
Wilson77Calixto <65840622+Wilson77Calixto@users.noreply.github.com>
2020-12-29 10:45:27 -0300

    Criação de matriz simplificada

---
+++ Estudo_Vetores_Criação_MatrizII.py
@@ -0,0 +1,10 @@
+# Estudo de Vetores. Criar Mtriz II
+
+n = int(input('Digite a dimensão n da matriz: '))
+m = int(input('Digite a dimensão m da matriz: '))
+matriz = []
+
+for i in range(n):
+    matriz.append([0]*m)
+
+print(matriz)    
